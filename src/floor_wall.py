2ce6a75da4e97b8dad457547ec5ea0bddd234c92
Robin Kristiansen <1232179+nibrobb@users.noreply.github.com>
2021-04-30 16:50:10 +0200

    Added ability to change color of floor/wall objects

--- src/floor_wall.py
+++ src/floor_wall.py
@@ -1,4 +1,5 @@
 """ floor_wall.py """
+from typing import Tuple
 import pygame
 
 from config import *
@@ -7,12 +8,19 @@ from static_object import StaticObject
 class FloorWall(StaticObject):
     """ Create a boundry, wall or floor, depending on you disposition """
     group = pygame.sprite.Group()
-    def __init__(self, width, height, pos):
+    def __init__(self, width, height, pos, color : Tuple[int, int, int] = COLOR_WHITE):
         super().__init__()
-        floor_wall_img = pygame.Surface([width, height])
-        # pygame.draw.rect(floor_wall_img, ARYAN, (0, 0, width, height))
-        floor_wall_img.fill(COLOR_WHITE)
-        self.image = floor_wall_img
-        self.rect = self.image.get_rect(x=pos[0]-width, y=pos[1]-height)
+        
+        self.width = width
+        self.height = height
         self.pos = pos
+        self.color = color
+
+        # Draw a white filled rectangle width the specified width and height
+        floor_wall_img = pygame.Surface([self.width, self.height])
+        floor_wall_img.fill(self.color)
+
+        self.image = floor_wall_img
+        
+        self.rect = self.image.get_rect(x=pos[0]-width, y=pos[1]-height)
         FloorWall.group.add(self)
